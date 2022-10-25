4544020e823e6aa010eca030a33de557dfd70a68
ArtemiyKhoba <87971469+ArtemiyKhoba@users.noreply.github.com>
2022-10-25 13:19:22 +0000

    Lec3

---
+++ lec_3_generators.py
@@ -0,0 +1,10 @@
+#np.arrange(start, stop, step)
+#np.linspace(start, stop, num)
+import numpy as np
+
+
+b = np.arange(0, 5, 0.1)
+print(b)
+
+d = np.linspace(0, 10, 11)
+print(d) 
---
+++ lec_3_math.py
@@ -0,0 +1,11 @@
+import math
+
+a = math.sin(3 * math.pi / 5)
+print(a)
+
+c = math.log10(42)
+print(c)
+
+d = math.asin(math.tan(math.pi / 4)) * 180 / math.pi
+
+print(d)
