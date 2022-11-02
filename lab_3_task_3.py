60809b223cf9bce8071b36ce3c1b465eedd92eff
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-11-02 17:45:29 +0200

    закончил лабу 3

--- lab_3_task_3.py
+++ lab_3_task_3.py
@@ -1,10 +1,23 @@
 from lab_3_task_1 import gravity_acceleration as g
-x0 = int(input("Введите начальную координату Х : "))
-y0  = int(input("Введите начальную координату Y : "))
-v0 = int(input("Введите начальную скорость V0 : "))
-print('t  x  y')
-for t in range(6):
-    x = x0 + v0 * t 
-    y = y0 + v0 * t + (g * t ** 2) / 2
-    print(t, x, y, sep = '  ', end = '\t')
-    print()
+import numpy as np 
+x0 = int(input('Input x0 : '))
+y0 = int(input('Input y0 : '))
+v0 = int(input('Input v0 : '))
+a = np.degrees(int(input('Input alpha : ')))
+vx0 = v0 * np.cos(np.radians(a))
+vy0 = v0 * np.sin(np.radians(a))
+
+
+t = np.arange(0, 5, 0.01)
+x = x0 + vx0 * t
+y = y0 + vy0 * t + g * t ** 2 / 2
+
+coord = np.zeros((len(t), 3))
+
+for i in range(len(t)): 
+
+    coords[i, 0] = t[i]
+    coords[i, 1] = x[i]
+    coords[i, 2] = y[i]
+
+print(coords)
