fbfe69ba186e263518f78fe70811396b7eb683c5
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-11-05 18:21:45 +0200

    l4

--- lab_4_task_2.py
+++ lab_4_task_2.py
@@ -2,10 +2,7 @@ import numpy as np
 
 
 def product(arr: np.ndarray): 
-    s = 1
-    for i in range(len(arr)): 
-        s *= arr[i]
-    return s
+    return arr.prod()
 
 arr = np.array([int(i) for i in input('Input array : ').split()])
 print(f'Product : {product(arr)}')
