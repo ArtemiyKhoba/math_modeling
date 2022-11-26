e21c79621372aad7d8df8d07040d008941ae6fbb
paofic <87971469+ArtemiyKhoba@users.noreply.github.com>
2022-11-26 14:06:00 +0200

    Delete lab_4_task_1.py

--- lab_4_task_1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def middle(arr: np.ndarray[int]) -> float:
-    return arr.mean()
-
-arr = np.array([int(i) for i in input('Input array : ').split()])
-print(f'Arithmetical mean : {middle(arr)}')
-
