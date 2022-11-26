24ea19f30f64ec50db25ba72933231155923c9a3
paofic <87971469+ArtemiyKhoba@users.noreply.github.com>
2022-11-26 14:07:48 +0200

    Delete lab_4_task_2.py

--- lab_4_task_2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def product(arr: np.ndarray): 
-    return arr.prod()
-
-arr = np.array([int(i) for i in input('Input array : ').split()])
-print(f'Product : {product(arr)}')
