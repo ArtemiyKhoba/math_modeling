0a68d651399bbd8062a890f78fa840107cb74bf6
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-11-26 16:15:28 +0200

    .

---
+++ lab_6_task_2.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def plotter(x0, x1, N, *args, **kwrgs): 
+    X = np.arange(x0, x1, N)
+
+    if kwrgs['obj'] == 'parabola':
+        y = args[0] * X ** 2 + args[1] * X + args[2]
+        title = 'parabola'
+    elif kwrgs['obj'] == 'hyperbola':
+        i = np.arange(0)
+        np.delete(X, i)
+        y = args[0] / X + args[1]
+        title = 'hyperbola'
+    
+    plt.plot(X, y)
+    plt.xlabel('coord: x')
+    plt.ylabel('coord: y')
+    plt.title(title)
+    plt.axis('equal')
+    plt.savefig('pic_1.png')
+
+
+plotter(-10, 10, 0.1, 1, 0,  obj = 'hyperbola')
+
+    
+
+
+
+
---
+++ lab_6_task_3.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def plotter(x0, x1, N, *args, **kwrgs): 
+    X = np.arange(x0, x1, N)
+
+    if kwrs['obj'] == 'circle':
+
+    elif kwrgs['obj'] == 'ellips':
+        
