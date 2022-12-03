e7b30d96a0f99cbd6ea2847396f615135476052b
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-12-03 14:44:37 +0200

    /

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -1,12 +1,28 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-def plotter(x0, x1, N, *args, **kwrgs): 
-    if kwrs['obj'] == 'circle':
-        x = np.arange(-2 * args[0], 2 * args[0], 0.1)
-        y = np.arange(-2 * args[0], 2 * args[0], 0,1)
-        X, Y = np.mashgrid(x, y)
-        
+def plotter(x0, x1, N, *args, **kwrgs):
+    if kwgrs['obj'] == 'circle':
+        x = np.arange(-2 * args[0], 2 * args[0], N)
+        y = np.arange(-2 * args[0], 2 * args[0], N)
+        X, Y = np.meshgrid(x, y)
 
+        fxy = X ** 2 + Y ** 2 - args[0] ** 2
+
+        plt.contour(X, Y, fxy, levels = [0])
+    
     # elif kwrgs['obj'] == 'ellips':
-        
+    #     x = np.arange(-2 * args[0], 2 * args[0], N)
+    #     y = np.arange(-2 * args[1], 2 * args[1], N)
+    #     X, Y = np.meshgrid(x, y)
+
+    #     fxy = (X/args[0]) ** 2 - (Y/args[1]) ** 2 - 1
+
+    #     plt.contour(X, Y, fxy, levels = [0])
+
+    plt.axis('equal')
+    plt.savefig('pic_1.png')
+
+
+plotter(1, 4, 5, 5, obj = 'circle')  
+
