ad2b0a0f42bc131b43d7d10e6ca646e28741b936
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-12-03 14:58:59 +0200

    .

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def plotter(x0, x1, N, *args, **kwrgs):
-    if kwgrs['obj'] == 'circle':
+    if kwrgs['obj'] == 'circle':
         x = np.arange(-2 * args[0], 2 * args[0], N)
         y = np.arange(-2 * args[0], 2 * args[0], N)
         X, Y = np.meshgrid(x, y)
@@ -11,18 +11,18 @@ def plotter(x0, x1, N, *args, **kwrgs):
 
         plt.contour(X, Y, fxy, levels = [0])
     
-    # elif kwrgs['obj'] == 'ellips':
-    #     x = np.arange(-2 * args[0], 2 * args[0], N)
-    #     y = np.arange(-2 * args[1], 2 * args[1], N)
-    #     X, Y = np.meshgrid(x, y)
+    elif kwrgs['obj'] == 'ellips':
+        x = np.arange(-2 * args[0], 2 * args[0], N)
+        y = np.arange(-2 * args[1], 2 * args[1], N)
+        X, Y = np.meshgrid(x, y)
 
-    #     fxy = (X/args[0]) ** 2 - (Y/args[1]) ** 2 - 1
+        fxy = (X/args[0]) ** 2 - (Y/args[1]) ** 2 - 1
 
-    #     plt.contour(X, Y, fxy, levels = [0])
+        plt.contour(X, Y, fxy, levels = [0])
 
     plt.axis('equal')
     plt.savefig('pic_1.png')
 
 
-plotter(1, 4, 5, 5, obj = 'circle')  
+plotter(1, 4, 5, 5, obj='circle')  
 
