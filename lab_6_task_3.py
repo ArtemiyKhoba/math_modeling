d5730cc0a34e794801a25329399d198878459cf7
ArtemiyKhoba <khoba.artemiy@icloud.com>
2022-12-03 13:56:12 +0200

    .

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def plotter(x0, x1, N, *args, **kwrgs): 
-    X = np.arange(x0, x1, N)
-
     if kwrs['obj'] == 'circle':
+        x = np.arange(-2 * args[0], 2 * args[0], 0.1)
+        y = np.arange(-2 * args[0], 2 * args[0], 0,1)
+        X, Y = np.mashgrid(x, y)
+        
 
-    elif kwrgs['obj'] == 'ellips':
+    # elif kwrgs['obj'] == 'ellips':
         
